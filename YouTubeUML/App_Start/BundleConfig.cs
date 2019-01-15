de26de04ab9feec79437814b4aa376c0aa144d86
Sckipper <Sckipper>
2019-01-15 19:46:24 +0200

    first commit

---
+++ YouTubeUML/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Optimization;
+
+namespace YouTubeUML
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            BundleTable.EnableOptimizations = true;
+
+            bundles.Add(new StyleBundle("~/css/layout").Include(
+                "~/Content/CSS/dashboard.css",
+                "~/Content/CSS/popuo-box.css",
+                "~/Content/CSS/style.css",
+                "~/Content/CSS/Layout.css"
+            ));
+
+            bundles.Add(new ScriptBundle("~/js/scripts").Include(
+                "~/Content/JS/bootstrap.min.js",
+                "~/Content/JS/jquery.magnific-popup.js",
+                "~/Content/JS/jquery.polyglot.language.switcher.js",
+                "~/Content/JS/jquery-1.11.1.min.js",
+                "~/Content/JS/modernizr.custom.min.js",
+                "~/Content/JS/responsiveslides.min.js"
+            ));
+        }
+    }
+}
---
+++ YouTubeUML/Content/JS/Proxy.js
@@ -0,0 +1,60 @@
+﻿function Proxy() {
+
+    $.ajax({
+        accepts: {
+            mycustomtype: 'application/x-some-custom-type'
+        },
+
+        // Instructions for how to deserialize a `mycustomtype`
+        converters: {
+            'text mycustomtype': function (result) {
+                // Do Stuff
+                return newresult;
+            }
+        },
+
+        // Expect a `mycustomtype` back from server
+        dataType: 'mycustomtype'
+    });
+
+    this.login = function (username, password) {
+
+    }
+
+    this.signOut = function () {
+
+    }
+
+    this.register = function (username, password, email, userRole) {
+
+    }
+
+    this.share = function () {
+
+    }
+
+    this.uploadVideo = function (fileName, filePath, fileSize, fileExtension) {
+    }
+
+    this.getVideo = function (videoId) {
+
+    }
+
+    this.addComment = function (comment, videoId) {
+
+    }
+
+    this.likeVideo = function (videoId) {
+
+    }
+
+    this.dislikeVideo = function (videoId) {
+
+    }
+
+    this.logAction = function (username, action) {
+
+    }
+};
+
+proxy = Proxy();
