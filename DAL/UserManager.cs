e52a00a30625fda35ed5b1eb4b23c18dea5dff56
Sckipper <Sckipper>
2019-01-15 22:03:07 +0200

    added DB

---
+++ DAL/UserManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class UserManager
+    {
+        public static bool CreateUser(String username, String password, int type)
+        {
+            using (var db = new YouTubeUMLEntities())
+            {
+                var user = new User();
+                db.Users.Add(user);
+                user.UserName = username;
+                user.Password = password;
+                user.Type = type;
+
+                db.SaveChanges();
+            }
+            return true;
+        }
+    }
+}
