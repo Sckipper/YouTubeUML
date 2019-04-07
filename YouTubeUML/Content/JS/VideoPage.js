e5b77e2ce9ad2e94635ae0d71d2a41bd1dbe3bf8
Sckipper <Sckipper>
2019-04-07 18:33:49 +0300

    videoPage modifications

--- YouTubeUML/Content/JS/VideoPage.js
+++ YouTubeUML/Content/JS/VideoPage.js
@@ -1,15 +1,20 @@
 ﻿var VideoPage = function() {
     var elements = {
-        uploadButton: $("#uploadButton"),
-        signInButton: $("#signIn"),
-        signOutButton: $("#signUp"),
-        logOutButton: $("#logOutButton"),
-        loginButton: $("#Login"),
-        username: $("#usernameInput"),
-        password: $("#passwordInput"),
-        usernameLabel: $("#usernameLabel")
+        likeButton: $("#likeButton"),
+        dislikeButton: $("#dislikeButton"),
+        videoId: $("#videoId")
     }
 
+    elements.likeButton.click(function () {
+        facade.likeVideo(elements.videoId.attr("data"))
+        elements.likeButton.html(Number(elements.likeButton.html()) + 1)
+    });
 
+    elements.dislikeButton.click(function () {
+        facade.dislikeVideo(elements.videoId.attr("data"))
+        elements.dislikeButton.html(Number(elements.dislikeButton.html()) + 1)
+    });
 
 }
+
+var vp = new VideoPage();
